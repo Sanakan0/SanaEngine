ff8f178d61c15c800cf273bae240c81c678b9a2c
SANAKAN0 <sanakan0@outlook.com>
2023-03-02 01:53:42 +0800

    tmp update

--- Render/include/SRender/LowRenderer/Camera.h
+++ Render/include/SRender/LowRenderer/Camera.h
@@ -10,16 +10,8 @@ namespace SRender::LowRenderer{
 class Camera {
 public:
 	Camera();
-	//trans in camera coord
-	void translate(glm::vec3 trans);
-	
-	void Orbit(float hori_deg,float verti_deg );
-	void CalcLookAt();
-	void FpsRotate(float hori_deg,float verti_deg);
-	void zoom(float step);
-	void reset();
-	void resetworldup();
-	float CalcDisPerPix(int w,int h); //Calc distance per pixel at the panel of cnterpoint
+
+
 	glm::mat4 CalcViewMat(const glm::vec3& pos,const glm::quat& orien) const;
 	glm::mat4 CalcProjectionMat(int w,int h) const;
 	const glm::mat4& GetViewMat() const{return view_;};
