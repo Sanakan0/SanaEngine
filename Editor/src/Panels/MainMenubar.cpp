83798fa57e6d6c97dd613ddedf3d4ecd8645aa76
NAVE726 <sanakan0@outlook.com>
2023-03-22 15:13:11 +0800

    add nativefiledialog lib for file open

--- Editor/src/Panels/MainMenubar.cpp
+++ Editor/src/Panels/MainMenubar.cpp
@@ -1,10 +1,16 @@
 #include "imgui/imgui.h"
+#include "nfd.h"
 #include <SEditor/Panels/MainMenubar.h>
 namespace SEditor::Panels{
 
 void MainMenubar::DrawImpl(){
     if (ImGui::BeginMainMenuBar()){
         if (ImGui::BeginMenu("SanaEngine")){
+            if (ImGui::MenuItem("Load tile")){
+                nfdchar_t* output = NULL;
+                NFD_OpenDialog(NULL, NULL, &output);
+                free(output);
+            }
             ImGui::EndMenu();
         }
         if (ImGui::BeginMenu("Edit")){
